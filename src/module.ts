658fa9948b320ba3b2034be599f6c436d2e15f3f
Olumide <olumideawofeso@gmail.com>
2025-11-29 21:20:55 -0800

    Initial MVP
    I don't really plan on developing this any further.

--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,15 @@
 /**
- * This file contains the plugin template.
+ * This file contains the class DummyPlatform.
+ * This file is a modified version of the original module.ts from https://github.com/Luligu/matterbridge-webhooks.
  *
  * @file module.ts
  * @author Luca Liguori
- * @created 2025-06-15
- * @version 1.3.0
+ * @author Olumide Awofeso
+ * @version 0.0.1
  * @license Apache-2.0
  *
  * Copyright 2025, 2026, 2027 Luca Liguori.
+ * Copyright 2025 Olumide Awofeso.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,101 +23,117 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { bridgedNode, MatterbridgeDynamicPlatform, MatterbridgeEndpoint, onOffLight, onOffOutlet, onOffSwitch, PlatformConfig, PlatformMatterbridge } from 'matterbridge';
+import { AnsiLogger } from 'matterbridge/logger';
 
-import { MatterbridgeDynamicPlatform, MatterbridgeEndpoint, onOffOutlet, PlatformConfig, PlatformMatterbridge } from 'matterbridge';
-import { AnsiLogger, LogLevel } from 'matterbridge/logger';
+export interface DummyConfig {
+  deviceType: 'Outlet' | 'Switch' | 'Light';
+  autoOffDelayMs: number;
+}
+
+export type DummyPlatformConfig = PlatformConfig & {
+  devices: Record<string, DummyConfig>;
+};
 
 /**
  * This is the standard interface for Matterbridge plugins.
  * Each plugin should export a default function that follows this signature.
  *
- * @param {PlatformMatterbridge} matterbridge - An instance of MatterBridge.
- * @param {AnsiLogger} log - An instance of AnsiLogger. This is used for logging messages in a format that can be displayed with ANSI color codes and in the frontend.
+ * @param {PlatformMatterbridge} matterbridge - An instance of MatterBridge. This is the main interface for interacting with the MatterBridge system.
+ * @param {AnsiLogger} log - An instance of AnsiLogger. This is used for logging messages in a format that can be displayed with ANSI color codes.
  * @param {PlatformConfig} config - The platform configuration.
- * @returns {TemplatePlatform} - An instance of the MatterbridgeAccessory or MatterbridgeDynamicPlatform class. This is the main interface for interacting with the Matterbridge system.
+ * @returns {Platform} - An instance of the SomfyTahomaPlatform. This is the main interface for interacting with the Somfy Tahoma system.
  */
-export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig): TemplatePlatform {
-  return new TemplatePlatform(matterbridge, log, config);
+export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: DummyPlatformConfig): DummyPlatform {
+  return new DummyPlatform(matterbridge, log, config);
 }
 
-// Here we define the TemplatePlatform class, which extends the MatterbridgeDynamicPlatform.
-// If you want to create an Accessory platform plugin, you should extend the MatterbridgeAccessoryPlatform class instead.
-export class TemplatePlatform extends MatterbridgeDynamicPlatform {
-  constructor(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig) {
-    // Always call super(matterbridge, log, config)
+export class DummyPlatform extends MatterbridgeDynamicPlatform {
+  private devices: Record<string, DummyConfig>;
+  readonly bridgedDevices = new Map<string, MatterbridgeEndpoint>();
+
+  constructor(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: DummyPlatformConfig) {
     super(matterbridge, log, config);
 
     // Verify that Matterbridge is the correct version
     if (this.verifyMatterbridgeVersion === undefined || typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.3.0')) {
-      throw new Error(
-        `This plugin requires Matterbridge version >= "3.3.0". Please update Matterbridge from ${this.matterbridge.matterbridgeVersion} to the latest version in the frontend."`,
-      );
+      throw new Error(`This plugin requires Matterbridge version >= "3.3.0". Please update Matterbridge to the latest version in the frontend.`);
     }
 
-    this.log.info(`Initializing Platform...`);
-    // You can initialize your platform here, like setting up initial state or loading configurations.
+    this.log.info('Initializing platform:', this.config.name);
+
+    this.devices = config.devices;
+
+    this.log.info('Finished initializing platform:', this.config.name);
   }
 
-  override async onStart(reason?: string) {
-    this.log.info(`onStart called with reason: ${reason ?? 'none'}`);
+  override async onStart(reason?: string): Promise<void> {
+    this.log.info('onStart called with reason:', reason ?? 'none');
 
-    // Wait for the platform to fully load the select if you use them.
+    // Clear all devices select
     await this.ready;
-
-    // Clean the selectDevice and selectEntity maps, if you want to reset the select. This is useful when you have an API that sends all the devices and you want to rediscover all of them.
     await this.clearSelect();
 
-    // Implements your own logic there
-    await this.discoverDevices();
-  }
+    // Register devices
+    let i = 0;
+    for (const deviceName in this.devices) {
+      this.log.debug(`Loading devices ${++i} ${deviceName}`);
 
-  override async onConfigure() {
-    // Always call super.onConfigure()
-    await super.onConfigure();
-
-    this.log.info('onConfigure called');
-
-    // Configure all your devices. The persisted attributes need to be updated.
-    for (const device of this.getDevices()) {
-      this.log.info(`Configuring device: ${device.uniqueId}`);
-      // You can update the device configuration here, for example:
-      // device.updateConfiguration({ key: 'value' });
+      const device = this.devices[deviceName];
+      this.setSelectDevice('device' + i, deviceName, undefined, 'hub');
+      if (!this.validateDevice(['device' + i, deviceName], true)) continue;
+      this.log.info(`Registering device: ${deviceName}`);
+      const matterDevice = new MatterbridgeEndpoint(
+        [device.deviceType === 'Outlet' ? onOffOutlet : device.deviceType === 'Light' ? onOffLight : onOffSwitch, bridgedNode],
+        { id: deviceName },
+        this.config.debug as boolean,
+      )
+        .createDefaultBridgedDeviceBasicInformationClusterServer(
+          deviceName,
+          'device' + i++,
+          this.matterbridge.aggregatorVendorId,
+          'Matterbridge',
+          'Matterbridge dummy',
+          0,
+          this.config.version as string,
+        )
+        .createOnOffClusterServer(false)
+        .addRequiredClusterServers()
+        .addCommandHandler('on', async () => {
+          this.log.info(`Device ${deviceName} on handler triggered.`);
+          if (device.autoOffDelayMs == 0) {
+            await matterDevice.setAttribute('onOff', 'onOff', false, matterDevice.log);
+          } else {
+            await matterDevice.setAttribute('onOff', 'onOff', true, matterDevice.log);
+          }
+          if (device.autoOffDelayMs > 0) {
+            setTimeout(async () => {
+              await matterDevice.setAttribute('onOff', 'onOff', false, matterDevice.log);
+            }, device.autoOffDelayMs);
+          }
+        })
+        .addCommandHandler('off', async () => {
+          this.log.info(`Device ${deviceName} off handler triggered.`);
+          await matterDevice.setAttribute('onOff', 'onOff', false, matterDevice.log);
+        });
+      await this.registerDevice(matterDevice);
+      this.bridgedDevices.set(deviceName, matterDevice);
     }
   }
 
-  override async onChangeLoggerLevel(logLevel: LogLevel) {
-    this.log.info(`onChangeLoggerLevel called with: ${logLevel}`);
-    // Change here the logger level of the api you use or of your devices
+  override async onConfigure(): Promise<void> {
+    await super.onConfigure();
+    this.log.info('onConfigure called');
+    this.bridgedDevices.forEach(async (device) => {
+      this.log.info(`Configuring device: ${device.deviceName}`);
+      await device.setAttribute('onOff', 'onOff', false, device.log);
+    });
   }
 
-  override async onShutdown(reason?: string) {
-    // Always call super.onShutdown(reason)
+  override async onShutdown(reason?: string): Promise<void> {
     await super.onShutdown(reason);
-
-    this.log.info(`onShutdown called with reason: ${reason ?? 'none'}`);
+    this.log.info('onShutdown called with reason:', reason ?? 'none');
     if (this.config.unregisterOnShutdown === true) await this.unregisterAllDevices();
-  }
-
-  private async discoverDevices() {
-    this.log.info('Discovering devices...');
-    // Implement device discovery logic here.
-    // For example, you might fetch devices from an API.
-    // and register them with the Matterbridge instance.
-
-    // Example: Create and register an outlet device
-    // If you want to create an Accessory platform plugin and your platform extends MatterbridgeAccessoryPlatform,
-    // instead of createDefaultBridgedDeviceBasicInformationClusterServer, call createDefaultBasicInformationClusterServer().
-    const outlet = new MatterbridgeEndpoint(onOffOutlet, { id: 'outlet1' })
-      .createDefaultBridgedDeviceBasicInformationClusterServer('Outlet', 'SN123456', this.matterbridge.aggregatorVendorId, 'Matterbridge', 'Matterbridge Outlet', 10000, '1.0.0')
-      .createDefaultPowerSourceWiredClusterServer()
-      .addRequiredClusterServers()
-      .addCommandHandler('on', (data) => {
-        this.log.info(`Command on called on cluster ${data.cluster}`);
-      })
-      .addCommandHandler('off', (data) => {
-        this.log.info(`Command off called on cluster ${data.cluster}`);
-      });
-
-    await this.registerDevice(outlet);
+    this.bridgedDevices.clear();
   }
 }
